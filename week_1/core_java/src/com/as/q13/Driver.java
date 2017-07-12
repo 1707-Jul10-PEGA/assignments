05cc2fc11225c62696c06dd8510252da48741b7b
Andrew <asanders0324@gmail.com>
2017-07-12 17:47:57 -0400

    Completed question 13.

--- week_1/core_java/src/com/as/q13/Driver.java
+++ week_1/core_java/src/com/as/q13/Driver.java
@@ -15,13 +15,28 @@ public class Driver {
 	 */
 	private static void printTriangle(int l){
 		//string representing the line to be printed
-		String line = "";
+		StringBuilder line = new StringBuilder("");
 		for (int i = 0; i < l; i++) {
+			//Initial line is a single 0
 			if (i == 0) {
-				line += "0";
+				line.append("0");
 			} else {
-				
+				//if the character at the beginning of the string
+				//is not equal to the character at the end of the string
+				//append the beginning character to the end of the string
+				if (line.charAt(0) != line.charAt(line.length() - 1)) {
+					line.append(" " + line.charAt(0));
+				} else {
+					//if the beginning character matches the ending character
+					//then insert the opposite character at the beginning
+					if ('0' == line.charAt(0)){
+						line.insert(0, "1 ");
+					} else {
+						line.insert(0, "0 ");
+					}
+				}
 			}
+			//print the line
 			System.out.println(line);
 		}
 	}
