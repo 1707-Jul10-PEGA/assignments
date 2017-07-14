ac6c7e2b3d67a193b58bf2f00299d18be02aec9e
Alexander Philip Quion <alexq.ph@gmail.com>
2017-07-14 15:44:34 -0400

    Fixed Q20

--- week_1/src/com/aq/q20/driver/ReadPeople.java
+++ week_1/src/com/aq/q20/driver/ReadPeople.java
@@ -6,32 +6,31 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 
-public class ReadInfo {
+public class ReadPeople {
 	public static void main(String[] args) {
-		String file = "info.txt";
+		String file = "people.txt";
 		String line = "";	
 		try {
 			FileReader fileReader = new FileReader(file);		
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			
 			String name = "";
 			String age = "";
 			String state = "";
 			
-			
 			while(((line = bufferedReader.readLine()) != null )) {
 				String[] fields = line.split(":");
-				
-				System.out.println(Arrays.toString(line.split(":")));
 
 				name = "Name: " + fields[0] + " " + fields[1];
 				age = "Age: " + fields[2] + " years";
-				state = "State: " + fields[3] + "State";
+				state = "State: " + fields[3] + " State";
 				
 				System.out.println(name);
 				System.out.println(age);
 				System.out.println(state);
+				System.out.println();
 			}
+			bufferedReader.close();
+			
 		}
 		catch(FileNotFoundException ex) {
 			System.out.println("Can't find the file at :"
