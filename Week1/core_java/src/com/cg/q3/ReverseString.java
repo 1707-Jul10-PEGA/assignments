77eef102e3ba99bda533a361f0afce30935e0939
Carlos Gastelum <carlosgstlm@gmail.com>
2017-07-14 20:55:16 -0400

    Made changes to problem 3

--- Week1/core_java/src/com/cg/q3/ReverseString.java
+++ Week1/core_java/src/com/cg/q3/ReverseString.java
@@ -1,31 +1,17 @@
 package com.cg.q3;
 
 public class ReverseString {
+	
+	/*
+	 * Reverse string by concating one character at a time
+	 * from right to the left
+	 */
 	public String reverseString(String str) {
 
 		for (int i = str.length() - 1; i >= 0; i--) {
 			str = str.concat(String.valueOf(str.charAt(i)));
 		}
 		return str.substring(str.length() / 2);
-
-	}
-	
-	public String reverseString1(String str) {
-		System.out.println(str);
-		if(str.length() == 1){
-			//System.out.println(str);
-			return str;
-		}else{
-			return reverseString1(str.substring(0, str.length()-1)) ;
-		}
-
-	}
-
-	public static void main(String[] args) {
-		ReverseString r = new ReverseString();
-
-		System.out.println(r.reverseString("Hello"));
-		System.out.println("String1 = " + r.reverseString1("Hello"));
 
 	}
 }
---
+++ Week1/core_java/src/com/cg/q3/ReverseStringDriver.java
@@ -0,0 +1,10 @@
+package com.cg.q3;
+
+public class ReverseStringDriver {
+	public static void main(String[] args) {
+		ReverseString r = new ReverseString();
+
+		System.out.println(r.reverseString("Hello"));
+
+	}
+}
